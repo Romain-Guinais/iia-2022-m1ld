d3732b6bd13429a9f3d12e5b7dbe31df5c67e91f
Jeremy <jeremy.perrouault@ascadis.fr>
2022-09-23 12:27:38 +0200

    JOUR 2 - CONFIG VIEWRESOLVER & MAPPINGS

--- JEE/PROJETS/formation-spring-web/src/main/java/fr/formation/controller/HomeController.java
+++ JEE/PROJETS/formation-spring-web/src/main/java/fr/formation/controller/HomeController.java
@@ -1,12 +1,19 @@
 package fr.formation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller // On demande à SPRING de la manager en tant que Controller Web
 public class HomeController {
-	@RequestMapping("/welcome")
-	public String welcome() {
-		return "/WEB-INF/views/welcome.jsp";
+//	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	@GetMapping("/welcome")
+	public String welcome(Model model) {
+		model.addAttribute("utilisateurSession", "test model Spring");
+		
+		return "welcome";
+//		return "/WEB-INF/views/welcome.jsp";
 	}
 }
