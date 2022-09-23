6a56dcf315ab7f147f7164cc63af98a9958f7260
Jeremy <jeremy.perrouault@ascadis.fr>
2022-09-23 13:47:10 +0200

    JOUR 2 - REQUEST PARAMS & DATA-BINDING

--- JEE/PROJETS/formation-spring-web/src/main/java/fr/formation/controller/HomeController.java
+++ JEE/PROJETS/formation-spring-web/src/main/java/fr/formation/controller/HomeController.java
@@ -3,17 +3,27 @@ package fr.formation.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import fr.formation.request.UtilisateurRequest;
 
 @Controller // On demande à SPRING de la manager en tant que Controller Web
 public class HomeController {
 //	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	@GetMapping("/welcome")
-	public String welcome(Model model) {
-		model.addAttribute("utilisateurSession", "test model Spring");
+	public String welcome(@RequestParam(required = false) String username, @RequestParam(required = false) Integer id, Model model) {
+		model.addAttribute("utilisateurSession", username);
+		System.out.println(id);
 		
 		return "welcome";
 //		return "/WEB-INF/views/welcome.jsp";
+	}
+	
+	@GetMapping("/welcome-binding")
+	public String welcomeBinding(UtilisateurRequest utilisateurRequest) {
+		System.out.println(utilisateurRequest.getUsername());
+		System.out.println(utilisateurRequest.getId());
+		
+		return "welcome";
 	}
 }
