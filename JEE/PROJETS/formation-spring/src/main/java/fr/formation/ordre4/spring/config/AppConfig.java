79f690e04c9c9a75d1068fc4cb07f9e4fdb4e6f3
Jeremy <jeremy.perrouault@ascadis.fr>
2022-09-23 11:10:14 +0200

    JOUR 2 - PROPERTIES

--- JEE/PROJETS/formation-spring/src/main/java/fr/formation/ordre4/spring/config/AppConfig.java
+++ JEE/PROJETS/formation-spring/src/main/java/fr/formation/ordre4/spring/config/AppConfig.java
@@ -2,9 +2,11 @@ package fr.formation.ordre4.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration // Classe de configuration SPRING
 @ComponentScan("fr.formation.ordre4.spring")
+@PropertySource("classpath:/musique.properties")
 public class AppConfig {
 
 //	@Bean // On demande à SPRING de gérer cette instance
--- JEE/PROJETS/formation-spring/src/main/java/fr/formation/ordre4/spring/musique/Ukulele.java
+++ JEE/PROJETS/formation-spring/src/main/java/fr/formation/ordre4/spring/musique/Ukulele.java
@@ -1,10 +1,14 @@
 package fr.formation.ordre4.spring.musique;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ukulele implements IInstrument {
+	@Value("${musique.instrument.son.ukulele}")
+	private String melodie;
+	
 	public String son() {
-		return "BLIM BLIM BLIM";
+		return this.melodie;
 	}
 }
