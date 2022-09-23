fe3dcfae31cc23055105eec28ade41c091b3d605
Jeremy <jeremy.perrouault@ascadis.fr>
2022-09-23 09:51:59 +0200

    JOUR 2 - INTRO SPRING CONFIG

--- JEE/PROJETS/formation-spring/src/main/java/fr/formation/ordre4/spring/Application.java
+++ JEE/PROJETS/formation-spring/src/main/java/fr/formation/ordre4/spring/Application.java
@@ -1,9 +1,23 @@
 package fr.formation.ordre4.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import fr.formation.ordre4.spring.musique.IInstrument;
+import fr.formation.ordre4.spring.musique.IMusicien;
+import fr.formation.ordre4.spring.config.AppConfig;
+
 public class Application {
 
 	public static void main(String[] args) {
+		// Chargement du contexte de SPRING
+		AnnotationConfigApplicationContext ctx =
+				new AnnotationConfigApplicationContext(AppConfig.class);
 		
+		// On extrait la Guitare du contexte de SPRING pour la manipuler
+		IMusicien guitariste = ctx.getBean("guitariste", IMusicien.class);
+		guitariste.jouer();
+		
+		// On ferme de contexte de SPRING
+		ctx.close();
 	}
-
 }
--- JEE/PROJETS/formation-spring/src/main/java/fr/formation/ordre4/spring/musique/Guitare.java
+++ JEE/PROJETS/formation-spring/src/main/java/fr/formation/ordre4/spring/musique/Guitare.java
@@ -1,6 +1,10 @@
 package fr.formation.ordre4.spring.musique;
 
 public class Guitare implements IInstrument {
+	public Guitare() {
+		System.out.println("CREATION DE LA GUITARE");
+	}
+	
 	public String son() {
 		return "GLINK GLINK GLINK";
 	}
